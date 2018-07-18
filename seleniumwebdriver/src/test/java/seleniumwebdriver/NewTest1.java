1c5f3e7623b7ba78defa3ad2596798ad5fd13af2
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-3W0KX52.dir.svc.accenture.com>
2018-07-18 11:41:20 +0530

    Commit at 11:41AM

--- seleniumwebdriver/src/test/java/seleniumwebdriver/NewTest1.java
+++ seleniumwebdriver/src/test/java/seleniumwebdriver/NewTest1.java
@@ -5,15 +5,32 @@ import static org.testng.Assert.assertEquals;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class NewTest1 {
+	@Parameters("browser")
 	@Test
-	public void testlogin()
+	
+	public void testlogin(String value)
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Training_B8C.07.19\\Downloads\\chromedriver_win32\\ChromeDriver.exe");
-	WebDriver driver =new ChromeDriver();
+	WebDriver driver=null;
+	if(value.equals("chrome"))
+	{
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Training_B8C.07.19\\Downloads\\chromedriver_win32\\ChromeDriver.exe");
+	driver =new ChromeDriver();
+	}
+	else if(value.equals("firefox"))
+	{
+	System.setProperty("webdriver.gecko.driver","C:\\Users\\Training_B8C.07.19\\Downloads\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+	driver =new FirefoxDriver();
+	}
+	else
+	{
+		System.out.println("No Driver Found");
+	}
 	driver.get("https://github.com/login");
 	driver.findElement(By.id("login_field")).sendKeys("divyasreeannareddy");
 	driver.findElement(By.id("password")).sendKeys("Bvbr3knj3");
